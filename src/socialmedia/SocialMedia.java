52b804bc795eb413096d1391748d6ec8e1dfcf2c
Priptide <priptidehelp@gmail.com>
2021-04-18 13:55:42 +0100

    User Work Through

---
+++ src/socialmedia/SocialMedia.java
@@ -0,0 +1,175 @@
+package socialmedia;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * SocialMedia is a compiling, functioning implementor of the
+ * SocialMediaPlatform interface.
+ * 
+ * @author Diogo Pacheco
+ * @version 1.0
+ */
+public class SocialMedia implements SocialMediaPlatform {
+
+	// Create an empty hashmap for our users
+	private Map<String, User> currentUsers;
+
+	public SocialMedia() {
+		this.currentUsers = new HashMap<>();
+	}
+
+	@Override
+	public int createAccount(String handle) throws IllegalHandleException, InvalidHandleException {
+		// Check the handle
+
+		// Update
+		return 0;
+	}
+
+	@Override
+	public int createAccount(String handle, String description) throws IllegalHandleException, InvalidHandleException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeAccount(int id) throws AccountIDNotRecognisedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeAccount(String handle) throws HandleNotRecognisedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void changeAccountHandle(String oldHandle, String newHandle)
+			throws HandleNotRecognisedException, IllegalHandleException, InvalidHandleException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateAccountDescription(String handle, String description) throws HandleNotRecognisedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String showAccount(String handle) throws HandleNotRecognisedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int createPost(String handle, String message) throws HandleNotRecognisedException, InvalidPostException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int endorsePost(String handle, int id)
+			throws HandleNotRecognisedException, PostIDNotRecognisedException, NotActionablePostException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int commentPost(String handle, int id, String message) throws HandleNotRecognisedException,
+			PostIDNotRecognisedException, NotActionablePostException, InvalidPostException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void deletePost(int id) throws PostIDNotRecognisedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String showIndividualPost(int id) throws PostIDNotRecognisedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public StringBuilder showPostChildrenDetails(int id)
+			throws PostIDNotRecognisedException, NotActionablePostException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getNumberOfAccounts() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTotalOriginalPosts() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTotalEndorsmentPosts() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getTotalCommentPosts() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMostEndorsedPost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMostEndorsedAccount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void erasePlatform() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void savePlatform(String filename) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loadPlatform(String filename) throws IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * This is used to validate a user's handle doesn't contain invalid characters
+	 * 
+	 * @param handle
+	 * @return The handle if validated
+	 * @throws InvalidHandleException When the handle isn't valid
+	 */
+	public String validateHandle(String handle) throws InvalidHandleException {
+
+	}
+
+	public boolean checkForHandle(String handle) {
+		return currentUsers.containsKey(handle);
+	}
+
+}
---
+++ src/socialmedia/User.java
@@ -0,0 +1,91 @@
+package socialmedia;
+
+public class User {
+    // Variables
+    private String handle;
+
+    private int id;
+
+    private String description;
+
+    // Constructors
+
+    /**
+     * Create a user with a handle and it's given id
+     * 
+     * @param handle
+     * @param id
+     */
+    public User(String handle, int id) {
+        this.handle = handle;
+        this.id = id;
+    }
+
+    /**
+     * Create a user wtih a handle, description and it's given id
+     * 
+     * @param handle
+     * @param id
+     * @param description
+     */
+    public User(String handle, int id, String description) {
+        this.handle = handle;
+        this.id = id;
+        this.description = description;
+    }
+
+    /**
+     * Gets the users description
+     * 
+     * @return The Users Description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets or updates the users description
+     * 
+     * @param description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets the users handle
+     * 
+     * @return The Users Handle
+     */
+    public String getHandle() {
+        return handle;
+    }
+
+    /**
+     * Sets or updates the users handle
+     * 
+     * @param handle
+     */
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    /**
+     * Gets the users current id
+     * 
+     * @return The Users Id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets or updates the users current id
+     * 
+     * @param id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
